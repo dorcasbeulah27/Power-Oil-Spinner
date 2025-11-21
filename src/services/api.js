c9548374346a3866e13b7b675d1650a6115fc93a
Beulah Dorcas <beulah.dorcas@tolaram.com>
2025-11-21 19:00:17 +0530

    removed server

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "https://poweroil-backend.onrender.com/api";
+ `${process.env.VITE_API_BASE_URL}/api` || "https://poweroil-backend.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import EnvironmentPlugin from "vite-plugin-environment";
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "https://poweroil-backend.onrender.com",
-        changeOrigin: true,
-      },
-    },
-  },
+  plugins: [
+    react(),
+    EnvironmentPlugin({
+      VITE_API_BASE_URL: "https://poweroil-backend.onrender.com",
+    }),
+  ],
+
 });
